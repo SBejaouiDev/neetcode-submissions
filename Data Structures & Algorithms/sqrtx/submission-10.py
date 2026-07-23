296884fa9d1669959b0f3c1349c915600fdceb36
Sebastian Bejaoui <sebastianbsocial@gmail.com>
2026-07-23 11:24:42 -0700

    Add: sqrtx - submission-10

---
+++ Data Structures & Algorithms/sqrtx/submission-10.py
@@ -0,0 +1,10 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        ## babylonian method 
+        r = x
+
+        while r * r > x: 
+            ## or it can be divided by two
+            r = (r + x // r) // 2
+
+        return r
