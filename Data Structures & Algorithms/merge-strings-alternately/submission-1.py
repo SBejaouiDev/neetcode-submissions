364aad914c871c3aa3d1a4446c8e793707bcaac9
Sebastian Bejaoui <sebastianbsocial@gmail.com>
2026-08-12 14:04:05 -0700

    Add: merge-strings-alternately - submission-1

---
+++ Data Structures & Algorithms/merge-strings-alternately/submission-1.py
@@ -0,0 +1,24 @@
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        """ 
+        Use two pointers and alternate between the two strings, appending to a new string
+
+        """
+
+        s1 = 0 
+        s2 = 0
+        newString = ""
+
+        while s1 < len(word1) or s2 < len(word2):
+            if s1 < len(word1):
+                newString += word1[s1]
+                
+            if s2 < len(word2):
+                newString += word2[s2]
+                
+            #newString += word1[s1] + word2[s2]
+            s1 += 1
+            s2 += 1
+
+        return newString
+            
