54956a8eb4b2df396c65aba078547c68d8acfd1a
Sebastian Bejaoui <sebastianbsocial@gmail.com>
2026-08-21 16:26:10 -0700

    Add: permutations-ii - submission-0

---
+++ Data Structures & Algorithms/permutations-ii/submission-0.py
@@ -0,0 +1,29 @@
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+
+
+        #PLAN 
+        res = []
+        perm = []
+        #booList = [False] * len(nums)
+        count = {n: 0 for n in nums}
+
+        for num in nums:
+            count[num] += 1
+
+        def dfs():
+            if len(perm) == len(nums):
+                res.append(perm.copy())
+                return
+            
+            #unsures no duplicates are found
+            for n in count:
+                if count[n] > 0:
+                    perm.append(n)
+                    count[n] -=1
+                    dfs()
+                    count[n] += 1
+                    perm.pop()
+
+        dfs()
+        return res
